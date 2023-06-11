21db4569e6e45c79a5be263a28f7268c78e402fd
FernandoJRR <fercitotron@gmail.com>
2023-06-10 19:50:10 -0600

    Add swagger package, add base configuration for swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('CICS-App Backend API')
+    .setDescription(
+      'Documentacion de los endpoints de la API del backend de la Aplicacion para Estudiantes de Ingenieria',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
